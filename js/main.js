c73b2a728fecfae1857ff51fda510e4d6ce9b268
Vladislav Keller <skiffvlad90@gmail.com>
2024-03-01 18:39:32 +0200

    jxgfjdg

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 console.log("1");
+function sayHello() {
+  console.log("hello");
+  
+}
 
 setTimeout(() => {
   console.log("2", "3");
 }, 6000);
+
+setTimeout(() => {
+  sayHello();
+}, 2000);
+
 console.log("3");
 console.log("4");
