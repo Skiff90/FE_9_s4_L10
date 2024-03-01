cee2919f7f6e6a2d170f16c134ee0b0125806ea5
Vladislav Keller <skiffvlad90@gmail.com>
2024-03-01 19:10:43 +0200

    фівфівфі

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
-console.log("1");
-function sayHello() {
-  console.log("hello");
+// 1st part
+
+// console.log("1");
+// function sayHello() {
+//   console.log("hello");
   
-}
+// }
 
-setTimeout(() => {
-  console.log("2", "3");
-}, 6000);
+// setTimeout(() => {
+//   console.log("2", "3");
+// }, 6000);
 
-setTimeout(() => {
-  sayHello();
-}, 2000);
+// setTimeout(() => {
+//   sayHello();
+// }, 2000);
 
-console.log("3");
-console.log("4");
+// console.log("3");
+// console.log("4");
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!2-nd part!!!!!!!!!!!!!!!!!!!!!!!!
+const greeting =() => {
+  console.log("hello");
+};
+
+// const timeldt = setTimeout(greeting, 2000);
+// user for cleat timer
+// clearTimeout(timeldt);
+
+const interval = setInterval(greeting,4,clearInterval(4))
+
